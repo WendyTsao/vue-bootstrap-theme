affaafec4aa52ae8ef4eee5c93d67748c0717824
Wendy <wendytsao84@gmail.com>
2021-05-04 00:50:44 +0800

    create theme data in

--- src/components/ThemeCard.vue
+++ src/components/ThemeCard.vue
@@ -27,7 +27,7 @@
           <li>
             <a
               href="https://themes.getbootstrap.com/product-category/admin-dashboard/"
-              >{{ subtitle }}</a
+              >{{ content.subtitle }}</a
             >
           </li>
         </ul>
