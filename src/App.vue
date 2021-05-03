08c51551a2398fd4c550920720032c91faaabb0e
Wendy <wendytsao84@gmail.com>
2021-05-03 23:33:57 +0800

    extract Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <router-view/>
+  <router-view></router-view>
+  <Footer></Footer>
 </template>
 
+<script>
+import Footer from "./components/Footer";
+export default {
+  components:{
+    Footer
+  }
+}
+</script>
+
 <style>
+
 </style>
