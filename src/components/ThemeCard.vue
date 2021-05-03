672c1f98271e9f3227ad7bd7216a231be48fc833
Wendy <wendytsao84@gmail.com>
2021-05-03 23:53:15 +0800

    extract themecard component

---
+++ src/components/ThemeCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="theme-card">
+    <div class="theme-card__body">
+      <a
+        class="d-block"
+        href="https://themes.getbootstrap.com/product/craft-bootstrap-5-admin-dashboard-theme/"
+      >
+        <img
+          data-src="https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-540x405.png"
+          data-srcset="https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-800x600.png 800w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail.png 1200w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-768x576.png 768w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-600x450.png 600w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-200x150.png 200w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-400x300.png 400w, https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-540x405.png 540w"
+          data-sizes="auto"
+          class="theme-card__img lazyautosizes lazyloaded"
+          sizes="240px"
+          srcset="
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-800x600.png  800w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail.png         1200w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-768x576.png  768w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-600x450.png  600w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-200x150.png  200w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-400x300.png  400w,
+            https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-540x405.png  540w
+          "
+          src="https://themes.getbootstrap.com/wp-content/uploads/2021/03/thumbnail-540x405.png"
+        />
+      </a>
+
+      <a
+        class="theme-card__body__overlay btn btn-brand btn-sm"
+        target="_blank"
+        href="https://themes.getbootstrap.com/preview/?theme_id=86954"
+        >Live preview</a
+      >
+    </div>
+    <div class="theme-card__footer">
+      <div class="theme-card__footer__item">
+        <a
+          class="theme-card__title mr-1"
+          href="https://themes.getbootstrap.com/product/craft-bootstrap-5-admin-dashboard-theme/"
+          >Craft – Bootstrap 5 Admin Dashboard Theme</a
+        >
+        <p class="theme-card__info"></p>
+        <ul class="prod_cats_list">
+          <li>
+            <a
+              href="https://themes.getbootstrap.com/product-category/admin-dashboard/"
+              >Admin &amp; Dashboard</a
+            >
+          </li>
+        </ul>
+        <p></p>
+      </div>
+      <div class="theme-card__footer__item">
+        <p class="theme-card__price">
+          <span class="woocommerce-Price-amount amount"
+            ><span class="woocommerce-Price-currencySymbol">$</span>49.00</span
+          >
+        </p>
+        <ul class="rating">
+          <li class="rating__item"></li>
+          <li class="rating__item"></li>
+          <li class="rating__item"></li>
+          <li class="rating__item"></li>
+          <li class="rating__item"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
